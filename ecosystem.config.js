b573f099673aaf96f9ca96bcbb92bbbcf54f288a
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-03-28 14:37:40 +0700

    perbaikan config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  apps : [{
-    name   : "wa-api",
-    script : "./multi.js"
+  apps: [{
+    name: "wa-api",
+    script: "./multi.js",
+    max_memory_restart: '1000M',
+    stop_exit_codes: [0],
+    instances: 1,
   }]
 }
