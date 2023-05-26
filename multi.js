e59ff40e2cd72ea9fa205a1006b7e2bde70e78ea
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-05-26 08:52:02 +0700

    perbaikan

--- multi.js
+++ multi.js
@@ -47,7 +47,6 @@ const setSessionsFile = function (sessions) {
 const getSessionsFile = function () {
   return JSON.parse(fs.readFileSync(SESSIONS_FILE));
 }
-
 const createSession = function (id, description, webhook) {
   console.log('Creating session: ' + id);
   const client = new Client({
@@ -237,6 +236,40 @@ app.get('/', (req, res) => {
   });
 });
 // send message
+app.post('/test', [
+  body('message').notEmpty(),
+], async (req, res) => {
+  const message = req.body.message;
+  // // send message
+  client.sendMessage("120363044576251255@g.us", message)
+      .then(
+          (response) => {
+              return res.send(response);
+          }
+      ).catch(
+          (error) => {
+              return res.send("Error : " + error);
+          }
+      );
+});
+// send notif group
+app.post('/notif', [
+  body('message').notEmpty(),
+], async (req, res) => {
+  const message = req.body.message;
+  // // send message
+  client.sendMessage("120363044576251255@g.us", message)
+      .then(
+          (response) => {
+              return res.send(response);
+          }
+      ).catch(
+          (error) => {
+              return res.send("Error : " + error);
+          }
+      );
+});
+// send message
 app.post('/send-message', [
   body('number').notEmpty(),
   body('username').notEmpty(),
@@ -317,6 +350,98 @@ app.post('/send-message-group', [
       response: err
     });
   });
+});
+// send list button
+app.post('/send-list', [
+  body('number').notEmpty(),
+  body('contenttext').notEmpty(),
+  body('buttontext').notEmpty(),
+  body('rowtitle').notEmpty(),
+], async (req, res) => {
+  // checking error
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.mapped() });
+  }
+  // init client
+  const number = phoneNumberFormatter(req.body.number);
+  let rowtitles = req.body.rowtitle;
+  // init list
+  let rows = [];
+  let i = 1;
+  // row title belakang koma
+  if (rowtitles.slice(-1) == ",") {
+      rowtitles = rowtitles.slice(0, -1)
+  }
+  // row desc null
+  if (req.body.rowdescription == null) {
+      rowdescription = '';
+  } else {
+      rowdescription = req.body.rowdescription.split(',');
+  }
+  rowtitles.split(',').forEach(element => {
+      let description = rowdescription[i - 1] || '';
+      let rowid = {
+          title: element,
+          description: description,
+          id: 'rowid' + i++,
+      };
+      rows.push(rowid);
+  });
+  const section = {
+      title: req.body.titlesection,
+      rows: rows,
+  };
+  const list = new List(req.body.contenttext, req.body.buttontext, [section], req.body.titletext, req.body.footertext)
+  await client.sendMessage(number, list)
+      .then(
+          (response) => {
+              console.log('SEND LIST');
+              return res.send(response);
+          }
+      ).catch(
+          (error) => {
+              console.log("Error : " + error);
+              return res.send("Error : " + error);
+          }
+      );
+});
+// send button
+app.post('/send-button', [
+  body('number').notEmpty(),
+  body('contenttext').notEmpty(),
+  body('buttontext').notEmpty(),
+], async (req, res) => {
+  // checking error
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.mapped() });
+  }
+  // init client
+  const number = phoneNumberFormatter(req.body.number);
+  // button button
+  let buttons = [];
+  let i = 1;
+  req.body.buttontext.split(',').forEach(element => {
+      let buttonid = {
+          body: element,
+          id: 'buttonid' + i++,
+      };
+      buttons.push(buttonid);
+  });
+  const buttons_reply = new Buttons(req.body.contenttext, buttons, req.body.titletext, req.body.footertext);
+  await client.sendMessage(number, buttons_reply)
+      .then(
+          (response) => {
+              console.log('SEND BUTTON');
+              return res.send(response);
+          }
+      ).catch(
+          (error) => {
+              console.log("Error : " + error);
+              return res.send("Error : " + error);
+          }
+      );
 });
 // send media
 app.post('/send-media', [
