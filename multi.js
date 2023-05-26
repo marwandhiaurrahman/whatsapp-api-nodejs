36993adc8c3f758a76318df7b3444cfc3157d435
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-05-26 10:37:30 +0700

    perbaikan

--- multi.js
+++ multi.js
@@ -232,6 +232,7 @@ app.post('/test', [
 ], async (req, res) => {
   const message = req.body.message;
   // // send message
+  const client = sessions.find(sess => sess.id == username)?.client;
   client.sendMessage("120363044576251255@g.us", message)
     .then(
       (response) => {
@@ -249,6 +250,7 @@ app.post('/notif', [
 ], async (req, res) => {
   const message = req.body.message;
   // // send message
+  const client = sessions.find(sess => sess.id == username)?.client;
   client.sendMessage("120363044576251255@g.us", message)
     .then(
       (response) => {
